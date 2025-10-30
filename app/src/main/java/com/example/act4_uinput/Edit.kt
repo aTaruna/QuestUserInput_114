315ba2b458388a7e414bf38a325522fac041f7d2
aTaruna <aTaruna@users.noreply.github.com>
2025-10-30 11:44:03 +0700

    membuat variabel untuk diingat dan disimpan

--- app/src/main/java/com/example/act4_uinput/Edit.kt
+++ app/src/main/java/com/example/act4_uinput/Edit.kt
@@ -1,2 +1,31 @@
 package com.example.act4_uinput
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun FormDataDiri(modifier: Modifier)
+{
+    //variabel mengingat nilai input dari keyboard
+    var textNama by remember  { mutableStateOf("") }
+    var textJK by remember  { mutableStateOf("") }
+    var textAlamat by remember  { mutableStateOf("") }
+
+    //variabel untuk menyimpan data dari komponen UI
+    var nama by remember  { mutableStateOf("") }
+    var jenis by remember  { mutableStateOf("") }
+    var alamat by remember  { mutableStateOf("") }
+
+    val gender:List<String> = listOf("Laki-laki","Perempuan")
+
+    Column (modifier = Modifier.padding(top = 50.dp),
+
+}
