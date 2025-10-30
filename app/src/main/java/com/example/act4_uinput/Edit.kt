d87bb24e6c3dee0bcd2409c9a1e2e8c19138daae
aTaruna <aTaruna@users.noreply.github.com>
2025-10-30 12:29:22 +0700

    menambah layout pembatas, button dan elevatedcard

--- app/src/main/java/com/example/act4_uinput/Edit.kt
+++ app/src/main/java/com/example/act4_uinput/Edit.kt
@@ -3,9 +3,15 @@ package com.example.act4_uinput
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -17,6 +23,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 
@@ -68,7 +77,29 @@ fun FormDataDiri(modifier: Modifier)
             onValueChange = {textAlamat = it}
         )
 
+        Divider(
+            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium),
+                top = dimensionResource(R.dimen.padding_medium)),
+            thickness = dimensionResource(R.dimen.divider_tipis),
+            color = Color.DarkGray)
+        Button(
+            modifier = Modifier.fillMaxWidth(1f),
+            enabled = textAlamat.isNotEmpty(),
+            onClick = {nama = textNama; jenis = textJK; alamat = textAlamat}
+        ) { Text(stringResource(R.string.submit)) }
+        Divider(
+            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium),
+                top = dimensionResource(R.dimen.padding_medium)),
+            thickness = dimensionResource(R.dimen.divider_tipis),
+            color = Color.DarkGray)
 
+        ElevatedCard (
+            elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.Black),
+            modifier = Modifier
+                .height(100.dp)
+                .width(300.dp)
+        )
     }
 
 }
