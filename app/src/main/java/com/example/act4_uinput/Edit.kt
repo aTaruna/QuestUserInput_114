657b844cca0eb79bbdeb473d09f7cf478a978c6e
aTaruna <aTaruna@users.noreply.github.com>
2025-10-30 11:49:46 +0700

    membuat layout column

--- app/src/main/java/com/example/act4_uinput/Edit.kt
+++ app/src/main/java/com/example/act4_uinput/Edit.kt
@@ -1,12 +1,19 @@
 package com.example.act4_uinput
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -27,5 +34,21 @@ fun FormDataDiri(modifier: Modifier)
     val gender:List<String> = listOf("Laki-laki","Perempuan")
 
     Column (modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally){
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+            label = {Text("Nama Lengkap")},
+            onValueChange = {textNama = it}
+        )
+        Row {
+            gender.forEach { item ->
+
+            }
+        }
+    }
 
 }
